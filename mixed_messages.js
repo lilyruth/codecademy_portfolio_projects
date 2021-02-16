4433adc14116c4e3d22a5b824d82dfb0e06ab9d8
lilyruth <doodlepath@gmail.com>
2021-02-16 15:00:43 -0600

    first commit readme and mixed_messages

---
+++ mixed_messages.js
@@ -0,0 +1,21 @@
+// Affirmation Machine
+
+// Part 1: Greeting
+
+// Part 2: Positive Name
+
+// Part 3: Action
+
+
+console.log("This program is in progress!");
+
+const affirmations = { 
+    greeting: ["Happy Day", "Greetings", "Hello", "Hi there"],
+    name: ["King or Queen", "Friend", "Star", "Wonderful Human", "Fabulous Person"],
+    action: ["Have an amazing day!", "You can do what is in front of you!", "You've got this!", "You have what it takes!", "Make it so!", "You are worthy of good things!", "Know your worth!", "You are fantastic!", "You deserve love!"]
+
+}
+
+function affirmationMachine() {
+ 
+}
